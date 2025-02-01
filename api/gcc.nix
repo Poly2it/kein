75c3069e7939c2aa04a9e4ec4b4046ec2a95e7e3
Poly2it <84731064+Poly2it@users.noreply.github.com>
2025-02-01 22:43:20 +0100

    Simplify constraint API logic

--- api/gcc.nix
+++ api/gcc.nix
@@ -1,25 +1,20 @@
-{ lib, ... }:
-
-{
-  include = package: unresolvedConstraints: let
-    constraints = lib.constraints.resolveConstraint unresolvedConstraints;
-  in constraints // {
-    include = (if (lib.hasAttr "include" constraints) then constraints.include else []) ++ [package];
-  };
-  link = name: unresolvedConstraints: let
-    constraints = lib.constraints.resolveConstraint unresolvedConstraints;
-  in constraints // {
-    link = (if (lib.hasAttr "link" constraints) then constraints.link else []) ++ [name];
-  };
-  setPositionIndependent = value: unresolvedConstraints: let
-    constraints = lib.constraints.resolveConstraint unresolvedConstraints;
-  in constraints // {
-    positionIndependent = value;
-  };
-  __functor = unresolvedConstraints: let
-    constraints = lib.constraints.resolveConstraint unresolvedConstraints;
-  in constraints // {
-    backend = "gcc";
-  };
+{ lib, ... }: let
+  updateConstraint = attr: f: default: unresolvedConstraints: let
+    constraints = (lib.constraints.resolveConstraint unresolvedConstraints);
+  in
+    lib.setAttr constraints attr (f (
+      if lib.hasAttr attr constraints then
+        lib.getAttr attr constraints
+      else default
+    ));
+  setConstraint = attr: value: unresolvedConstraints: let
+    constraints = (lib.constraints.resolveConstraint unresolvedConstraints);
+  in
+    lib.setAttr constraints attr value;
+in {
+  include                = package: c: c |> updateConstraint "include" (prev: prev ++ [package]) [];
+  link                   = package: c: c |> updateConstraint "link" (prev: prev ++ [package]) [];
+  setPositionIndependent = value: c: c |> setConstraint "positionIndependent" value;
+  __functor              = value: c: c |> setConstraint "backend" "gcc";
 }
 
